5079db46ab657d9a8fd454760d0da61efda113c8
Infarh <shmachilin@yandex.ru>
2021-12-20 20:39:52 +0300

    Модели

---
+++ WebApp/Data/TestData.cs
@@ -0,0 +1,15 @@
+﻿using WebApp.Models;
+
+namespace WebApp.Data;
+
+public class TestData
+{
+    public static ICollection<Product> Products { get; } = Enumerable.Range(1, 100)
+       .Select(i => new Product
+        {
+            Id = i,
+            Name = $"Product-{i}",
+            Price = i * 1000,
+        })
+       .ToList();
+}
