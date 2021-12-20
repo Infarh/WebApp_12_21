310f8dcc035f9dfbac0541d8ee2b9c343e917868
Infarh <shmachilin@yandex.ru>
2021-12-20 19:46:34 +0300

    Определение маршрута

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,7 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var services = builder.Services;
+services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+//app.MapDefaultControllerRoute();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
