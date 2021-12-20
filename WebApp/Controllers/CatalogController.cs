be3b69a910d5984333ab348da5f7af5f587dc3a0
Infarh <shmachilin@yandex.ru>
2021-12-20 21:06:59 +0300

    Редактирование данных

--- WebApp/Controllers/CatalogController.cs
+++ WebApp/Controllers/CatalogController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApp.Data;
 using WebApp.Models;
+using WebApp.ViewModels;
 
 namespace WebApp.Controllers;
 
@@ -18,5 +19,37 @@ public class CatalogController : Controller
     public IActionResult Index()
     {
         return View(_Products);
+    }
+
+    public IActionResult Edit(int id)
+    {
+        var product = _Products.FirstOrDefault(p => p.Id == id);
+        if (product is null)
+            return NotFound();
+
+        var model = new ProductViewModel
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Price = product.Price,
+        };
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(ProductViewModel Model)
+    {
+        if (!ModelState.IsValid)
+            return View(Model);
+
+        var product = _Products.FirstOrDefault(p => p.Id == Model.Id);
+        if (product is null)
+            return NotFound();
+
+        product.Name = Model.Name;
+        product.Price = Model.Price;
+
+        return RedirectToAction(nameof(Index));
     }
 }
---
+++ WebApp/ViewModels/ProductViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.ViewModels;
+
+public class ProductViewModel
+{
+    [HiddenInput(DisplayValue = false)]
+    public int Id { get; set; }
+
+    [Display(Name = "Название")]
+    [StringLength(255, MinimumLength = 5)]
+    //[DataType(DataType.Password)]
+    public string Name { get; set; }
+
+    [Display(Name = "Цена")]
+    [Range(0, double.PositiveInfinity)]
+    public decimal Price { get; set; }
+}
