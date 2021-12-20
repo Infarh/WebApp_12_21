a9cef2227b43767dc45c7eea02515f38c28d6e58
Infarh <shmachilin@yandex.ru>
2021-12-20 20:48:30 +0300

    Демонстрация каталога товаров

---
+++ WebApp/Controllers/CatalogController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+public class CatalogController : Controller
+{
+    private readonly ILogger<CatalogController> _Logger;
+    private readonly ICollection<Product> _Products;
+
+    public CatalogController(ILogger<CatalogController> Logger)
+    {
+        _Products = TestData.Products;
+        _Logger = Logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View(_Products);
+    }
+}
