1b8e1ac96872d18322ddc158fe5c6661b138648a
Infarh <shmachilin@yandex.ru>
2021-12-20 19:54:09 +0300

    Результаты, возвращаемые контроллером

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+//[Controller]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _Logger;
+
+    public HomeController(ILogger<HomeController> Logger) => _Logger = Logger;
+
+    public IActionResult Index()
+    {
+        //return StatusCode(123, new { Message = "Странный статусный код" });
+        //return BadRequest();
+        //return NotFound();
+        return View();
+        //return File()
+        //return new FileStreamResult()
+        //return Json(new { ServerTime = DateTime.Now, Message = "Hello World!" });
+        //return Content("Hello World!");
+    }
+
+    public DateTime ServerTime()
+    {
+        //HttpContext.Response.Headers["Time"] = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+        //HttpContext.Response.Cookies.Append("Time", DateTime.Now.ToString());
+        return DateTime.Now;
+    }
+}
